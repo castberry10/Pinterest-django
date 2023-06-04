a6eeaf7dc3fcdc5b49c26e1a37835b35917d08cc
최원영 <sable@kangwon.ac.kr>
2023-06-04 07:18:14 +0000

    accountapp Template set (block contnet,, & extend ..

--- accountapp/views.py
+++ accountapp/views.py
@@ -3,5 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def hello_world(request):
-    return render(request, 'base.html')
+    # return render(request, 'base.html')
+    return render(request, 'accountapp/helloworld.html')
 
