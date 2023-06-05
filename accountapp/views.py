15c47a240dfe33444efc0323ec7376ecf128d33d
최원영 <sable@kangwon.ac.kr>
2023-06-05 13:28:51 +0000

    POST를 사용한 DB 저장

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from accountapp.models import HelloWorld
 # from django.http import HttpResponse
 
 # Create your views here.
@@ -6,7 +7,13 @@ from django.shortcuts import render
 
 def hello_world(request):
     if request.method == "POST":
-        return render(request, 'accountapp/helloworld.html', context={'text': 'POST METHOD!'})
+        temp = request.POST.get('hello_world_input')  # 리퀘스트에서 Post 중에서 hello_world_input라는 데이터를 가져와라..
+        
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+        
+        return render(request, 'accountapp/helloworld.html', context={'hello_world_output': new_hello_world})
     else:
         return render(request, 'accountapp/helloworld.html', context={'text': 'GET METHOD!'})
 
