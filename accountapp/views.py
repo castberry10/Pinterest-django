cdf548c6d57aa25a178cce6009f1662271b8653d
최원영 <sable@kangwon.ac.kr>
2023-06-05 13:49:06 +0000

    Get data from DB / django template for loop

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
 from accountapp.models import HelloWorld
+from django.urls import reverse
 # from django.http import HttpResponse
 
 # Create your views here.
@@ -8,12 +10,14 @@ from accountapp.models import HelloWorld
 def hello_world(request):
     if request.method == "POST":
         temp = request.POST.get('hello_world_input')  # 리퀘스트에서 Post 중에서 hello_world_input라는 데이터를 가져와라..
-        
+
         new_hello_world = HelloWorld()
         new_hello_world.text = temp
         new_hello_world.save()
         
-        return render(request, 'accountapp/helloworld.html', context={'hello_world_output': new_hello_world})
+        return HttpResponseRedirect(reverse('accountapp:hello_world')) #accountapp에 있는 hello_world로 가달라
     else:
-        return render(request, 'accountapp/helloworld.html', context={'text': 'GET METHOD!'})
+
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/helloworld.html', context={'hello_world_list': hello_world_list})
 
