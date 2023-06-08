8c91e528bcdbed2751d87e2e82c63ac4d5d24d7e
최원영 <sable@kangwon.ac.kr>
2023-06-08 11:44:28 +0000

    DetailView

--- accountapp/views.py
+++ accountapp/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from accountapp.models import HelloWorld
 from django.urls import reverse, reverse_lazy
 # from django.http import HttpResponse
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
@@ -28,3 +28,7 @@ class AccountCreateView(CreateView):
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html' #회원가입
     
+class AccountDetailView(DetailView):
+    model = User
+    context_object_name = 'target_user'
+    template_name = 'accountapp/detail.html'
