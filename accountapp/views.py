563b2cfb9dbad15ba8267bc493a467b3f12bc3a5
최원영 <sable@kangwon.ac.kr>
2023-06-08 08:10:12 +0000

    Signup implementation with CreateView

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from accountapp.models import HelloWorld
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 # from django.http import HttpResponse
-
-# Create your views here.
-
+from django.views.generic import CreateView
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 def hello_world(request):
     if request.method == "POST":
@@ -14,10 +14,16 @@ def hello_world(request):
         new_hello_world = HelloWorld()
         new_hello_world.text = temp
         new_hello_world.save()
-        
+
         return HttpResponseRedirect(reverse('accountapp:hello_world')) #accountapp에 있는 hello_world로 가달라
     else:
 
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/helloworld.html', context={'hello_world_list': hello_world_list})
 
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/create.html' #회원가입
